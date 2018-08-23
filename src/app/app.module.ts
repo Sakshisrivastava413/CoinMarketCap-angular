f15df3b75357eac16803507fc4f8d53632e08a57
sakshi <sakshisrivastava413@gmail.com>
2018-08-23 15:00:09 +0530

    defining routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { DetailComponent } from './detail/detail.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    DetailComponent
+    routingComponent
   ],
   imports: [
     BrowserModule,
